82f4574d9edcc00c3c6186e3b435dd491d8df99f
juma <juma.durduyev@gmail.com>
2021-09-07 07:45:04 -0700

    Initial Commit

---
+++ Red Gradient/Red_GradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Red_GradientApp.swift
+//  Red Gradient
+//
+//  Created by Juma on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Red_GradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
